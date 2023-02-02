d17b3bea595fb7f7447bebc56785a5fc2f6c3ca7
kseter <kse.terekhova@gmail.com>
2023-02-02 20:12:41 +0300

    style: blocks; fix: js/html

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const handleEditButtonClick = () => {
-	popup.classList.toggle('popup_opened')
-};
-editButton.addEventListener('click', handleEditButtonClick);
-
 const closeButton = document.querySelector('.popup__close-button');
-const handleCloseButtonClick = () => {
-	popup.classList.toggle('popup_opened');
-};
-closeButton.addEventListener('click', handleCloseButtonClick);
 
-const saveButton = document.querySelector('.popup__save-button');
-const handleSaveButtonClick = () => {
-	popup.classList.remove('popup_opened')
-};
-saveButton.addEventListener('click', handleSaveButtonClick);
-
+const popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__user-name');
-let aboutInput = document.querySelector('.popup__user-about');
+let nameInput = document.querySelector('.popup__form-item_name');
+let aboutInput = document.querySelector('.popup__form-item_about');
+let userName = document.querySelector('.profile__user-name');
+let userAbout = document.querySelector('.profile__user-about');
+
+const handleEditButtonClick = () => {
+	popup.classList.add('popup_opened')
+};
+
+const handleCloseButtonClick = () => {
+	popup.classList.remove('popup_opened');
+};
+
 function handleFormSubmit(evt) {
 	evt.preventDefault();
 
-	nameInput.getAttribute('value');
-	aboutInput.getAttribute('value');
-
-	let userName = document.querySelector('.profile__user-name');
-	let userAbout = document.querySelector('.profile__user-about');
-
 	userName.textContent = nameInput.value;
 	userAbout.textContent = aboutInput.value;
+
+	handleCloseButtonClick();
 };
 
 formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', handleEditButtonClick);
+closeButton.addEventListener('click', handleCloseButtonClick);
 
